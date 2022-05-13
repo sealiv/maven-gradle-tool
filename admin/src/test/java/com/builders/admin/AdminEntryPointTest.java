91a0c53de7f103862f22ae6d660b7bb8473024b5
Oleksii Sergiienko <sai.79.ua@gmail.com>
2022-05-13 11:58:01 +0300

    Initial Gradle multi module project 'Builders'.

---
+++ admin/src/test/java/com/builders/admin/AdminEntryPointTest.java
@@ -0,0 +1,24 @@
+package com.builders.admin;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AdminEntryPointTest {
+
+    @Test
+    public void testHello() {
+        InputStream stream = AdminEntryPoint.class.getClassLoader().getResourceAsStream("admin_config_test.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(properties.getProperty("test_hello"));
+    }
+
+}
